a6ead04a844dc5ac2ab23c1f4d845b72ee4f3945
Jaydeep Baldaniya <jaydeepbaldaniya08@gmail.com>
2024-05-07 23:58:24 +0530

    learn Document Object Model

---
+++ index.js
@@ -0,0 +1,61 @@
+// // a1.concat(a2, a3);
+
+// // let obj = {
+// //   a: 1,
+// //   b: 2,
+// //   c: 3,
+// // };
+
+// // for (const key in obj) {
+// //   if (Object.hasOwnProperty.call(obj, key)) {
+// //     const element = obj[key];
+// //     console.log(key, element);
+// //   }
+// // }
+
+// // for (const value of a) {
+// //   console.log(value);
+// // }
+// let a = [2, 5, 44, 8, 95, 5];
+// // let b = [];
+
+// // for (let index = 0; index < a.length; index++) {
+// //   const element = a[index];
+// //   b.push(element * 2);
+// // }
+
+// // let b = a.map((e, index, array) => {
+// //   return e ** 3;
+// // });
+
+// // console.log(b);
+
+// const b = (e) => {
+//   if (e > 7) {
+//     return true;
+//   }
+//   return false;
+// };
+
+// console.log(a.filter(b));
+
+// let c = [1, 2, 3, 4, 5];
+
+// const red = (a, b) => {
+//   return a * b;
+// };
+
+// console.log(c.reduce(red));
+
+// document.getElementById('red').style.backgroundColor = 'red';
+
+// document.querySelector('.box').style.backgroundColor = 'blue';
+
+// document.querySelectorAll('.box').forEach((e) => {
+//   e.style.backgroundColor = 'green';
+// });
+
+// document.querySelectorAll('.box').forEach((e) => {
+//   e.style.backgroundColor = 'green';
+//   e.style.color = 'blue';
+// });
